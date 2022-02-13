72d09958d006298e8e07bc868a31ef115c506ee5
nail <malikov.kzn@gmail.com>
2022-02-13 21:05:09 +0300

    lesson 01 - init

---
+++ 01/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'classes/FirstClass.php';
+
+$o1 = new FirstClass;
+$o2 = new FirstClass;
+var_dump($o1);
+var_dump($o2);
+
+
+echo '<div style="margin: 20px;"><a href="/">back</a></div>';
